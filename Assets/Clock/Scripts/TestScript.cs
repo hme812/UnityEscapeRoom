8cf9fc50cbbf396f002aeb81ec44321820199eac
Mengen Huang <mengen.huang@gmail.com>
2016-03-22 16:37:24 -0400

    MiddleVRstrt

---
+++ Assets/Clock/Scripts/TestScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestScript : MonoBehaviour {
+	GameObject secondHand;
+	// Use this for initialization
+	void Start () {
+		secondHand = GameObject.Find ("SecondHand");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		secondHand.transform.Rotate (10, 0, 0);
+	}
+}
