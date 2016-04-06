62da171c8cf809f4b4d711e16a8cab00fe7b8cf4
Mengen Huang <mengen.huang@duke.edu>
2016-04-05 23:07:31 -0400

    FirstPuzzleWithMiddleVR
    Added middleVR and Sixense

---
+++ Assets/MagicMirror/DetectCollision.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DetectCollision : MonoBehaviour {
+    AudioSource audio;
+
+    void Start()
+    {
+        audio = GetComponent<AudioSource>();
+    }
+
+ //   void OnCOllisionEnter(Collision col){//Why this doesn't work??
+ //       print("Collide");
+	//	if (col.gameObject.name == "RedFlower") {
+	//		Destroy (col.gameObject);
+	//	}
+	//}
+
+ //   void OnCollisionEnter2D(Collision2D collision)
+ //   {
+ //       print("Collided!");
+ //       if (collision.gameObject.tag == "RedFlower")
+ //       {
+ //           Destroy(gameObject);
+ //       }
+ //   }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        print("Detected!");
+        foreach (ContactPoint contact in collision.contacts)
+        {
+            Debug.DrawRay(contact.point, contact.normal, Color.white);
+            print("Detected");
+            print(collision.gameObject.name);
+            if (collision.gameObject.name == "FrontWall")
+            {
+                print("play audio");
+                audio.Play();
+				//Destroy (this.gameObject);
+            }
+        }
+
+
+    }
+
+}
